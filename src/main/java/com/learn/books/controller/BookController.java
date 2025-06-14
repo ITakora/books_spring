be51831abe385d2fb15219024dc530a236ca354f
ITakora <oyamitakora@gmail.com>
2025-06-14 21:42:36 +0700

    adding entity and setup

--- src/main/java/com/learn/books/controller/BookController.java
+++ src/main/java/com/learn/books/controller/BookController.java
@@ -1,14 +1,32 @@
 package com.learn.books.controller;
 
 
+import com.learn.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/api")
-    public String firstApi () {
-        return "Hello World";
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("Title one", "John", "Comedy"),
+                new Book("Title Two", "Chen", "Action"),
+                new Book("Title Three", "Sebrina", "Action")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks () {
+        return books;
     }
 }
---
+++ src/main/java/com/learn/books/entity/Book.java
@@ -0,0 +1,19 @@
+package com.learn.books.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+
+    private String title;
+    private String author;
+    private String category;
+
+    public Book(String title, String author, String category) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    }
+}
